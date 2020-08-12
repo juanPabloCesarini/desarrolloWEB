cf780ba782ba7b8c7a52be6e22d6f8176e3bc7c3
juanPabloCesarini <jpablocesarini@gmail.com>
2020-08-11 21:37:13 -0300

    por seguridad se eliminan los datos reales del archivo envioMail.php

--- php/tp1/envioMail.php
+++ php/tp1/envioMail.php
@@ -11,28 +11,28 @@
 
         $mail = new phpmailer();
         $mail->PluginDir = "../../includesMails/";
- 
+
         try {
             //Server settings
-            
+
             //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
             //$mail->SMTPDebug = 2;                                    // Enable verbose debug output
             $mail->isSMTP();                                           // Send using SMTP
             $mail->SMTPAuth   = true;                                  // Enable SMTP authentication
-            $mail->Host       = 'mail.p4000324.ferozo.com';            // Set the SMTP server to send through
-            $mail->Username   = 'info@artigaspublicidad.com.ar';       // SMTP username
-            $mail->Password   = 'kDmH@Cv8oX';                          // SMTP password
+            $mail->Host       = ' ';            // Set the SMTP server to send through
+            $mail->Username   = ' ';       // SMTP username
+            $mail->Password   = ' ';                          // SMTP password
             // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             // $mail->SMTPSsecure='tls';                               // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            // $mail->SMTPAutoTLS = false;  
+            // $mail->SMTPAutoTLS = false;
             $mail->Port       = 587;                                   // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
-            $mail->SetFrom('info@artigaspublicidad.com.ar', 'IFTS-16 - Friends Software Factory');
-           // $mail->AddAddress('mcferrocroce@gmail.com');             // Add a recipient
-			$mail->AddCC("natalialarossa@gmail.com");	               // Para copiar a alguien
-            $mail->AddCC("sebagrillo86@gmail.com");
-            $mail->AddCC("jpablocesarini@gmail.com");
+            $mail->SetFrom(' ');
+            $mail->AddAddress(' ');             // Add a recipient
+	    $mail->AddCC(" ");	               // Para copiar a alguien
+            $mail->AddCC(" ");
+            $mail->AddCC(" ");
 
             // Attachments
             //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
--- php/tp2/PHPClases/conexion.php
+++ php/tp2/PHPClases/conexion.php
@@ -6,10 +6,10 @@
         private function __construct() { }
 
         public static function conectar(){
-            $user='root';
-            $pass='';
+            $user='p4000324';
+            $pass='panTa19gRue91LiKo';
             $pdo_optional[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-            self::$conexion=new PDO('mysql:host=localhost;dbname=dbtareas',$user,$pass,$pdo_optional);
+            self::$conexion=new PDO('mysql:host=localhost;dbname=p4000324_tareas',$user,$pass,$pdo_optional);
             return self::$conexion;
         }   
     }
